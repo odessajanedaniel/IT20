3a7311fab706391788da1923d9af389b8a185d3a
odessajanedaniel <dessangdaniel@gmail.com>
2025-11-20 15:28:22 +0800

    added do-while-loops

---
+++ IT20C/Looping.js
@@ -0,0 +1,32 @@
+// For looping
+
+for(i = 0; i<=5 ; i ++){
+    output += i + " ";
+
+}
+
+console.log(output);
+
+//Arrays
+output = " ";
+arr = [1,2,3]
+
+for (index =0 ; index <arr.length; index++ ){
+    //console.log
+    output += arr[index];
+}
+
+console.log(output);
+
+
+
+output = " ";
+arr = [1,2,3]
+console.log ("Array length: + arr.length");
+
+for(index = arr.length -1; index>=0; index--){
+    //console.log(arr [index])
+    output += arr [index] +" ";
+}
+
+console.log(output);
---
+++ IT20C/while-loop.js
@@ -0,0 +1,46 @@
+console.log("reverse");
+
+i = 5;
+ while (i <= 5){
+    console.log("ascending");
+    x = 1;
+
+    while (x >= 5){
+        console.log(x);
+        i++;
+
+    }
+
+    console.log (output);
+    
+    arr=[1,2,3];
+    output += x+" ";
+
+}
+
+console.log (output);
+
+ arr=[4,2,3];
+ output += x+" ";
+ index = 1;
+
+ while(index <= arr.length){
+    output += index + " ";
+    index =0
+
+ }
+
+ console.log(output);
+
+
+ arr = [4,2,3];
+ index = 0;
+ output = "";
+
+ do{
+    output += arr[index] + " ";
+    index++;
+ } while(index < arr.length);
+
+ console.log(output);
+
