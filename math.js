50d7dea0a19325381ef99dca27aeacfe224a152c
odessajanedaniel <dessangdaniel@gmail.com>
2025-12-01 12:40:07 +0800

    added new queue

---
+++ math.js
@@ -0,0 +1,31 @@
+num1 =2;
+num2 =3;
+
+sum(2,3);
+function sum(num1,num2){
+    sum= num1 + num2;
+      console.log(sum);
+}
+ diff(2,3);
+function diff(num1,num2){
+    diff= num1 - num2;
+      console.log(diff);
+}
+
+ prod(2,3);
+function prod(num1,num2){
+    prod= num1 * num2;
+      console.log(prod);
+}
+
+quotient(2,3);
+function quotient(num1,num2){
+    quotient= num1 / num2;
+      console.log(quotient);
+}
+
+modulus(2,3);
+function modulus(num1,num2){
+    modulus= num1 % num2;
+      console.log(modulus);
+}
